fbc1da5b6acb02315d62f69c6151a56988fd55b9
Billy <47019848+VexA3@users.noreply.github.com>
2021-08-29 13:57:55 -0500

    removing some of the text for example

--- src/com/CoffeeQuest/Main.java
+++ src/com/CoffeeQuest/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
 
-        // some code about how movement works
+        // some code about how 
     }
 }
