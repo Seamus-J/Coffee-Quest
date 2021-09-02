d5d673bf3dc8145b51c3ffe186a0b3249e783a5e
Jared-Wad <89890283+Jared-Wad@users.noreply.github.com>
2021-09-02 17:35:01 -0500

    Remove Print Test

--- src/com/CoffeeQuest/Main.java
+++ src/com/CoffeeQuest/Main.java
@@ -5,7 +5,5 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         //james made a change
-        //Jared also made a change
-        System.out.println("Test");
     }
 }
