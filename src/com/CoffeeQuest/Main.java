0ebb96878bdc2da6724f2f32f0193448574b44ae
Jared-Wad <89890283+Jared-Wad@users.noreply.github.com>
2021-09-02 17:23:31 -0500

    Print Test

--- src/com/CoffeeQuest/Main.java
+++ src/com/CoffeeQuest/Main.java
@@ -5,5 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         //james made a change
+        //Jared also made a change
+        System.out.println("Test");
     }
 }
