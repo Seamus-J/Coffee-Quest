6f32647e13014cfae0022a4f3b99d0eab2fd58df
Billy <47019848+VexA3@users.noreply.github.com>
2021-09-05 14:28:07 -0500

    Added basic commands and a start to the playable game

--- src/com/CoffeeQuest/Main.java
+++ src/com/CoffeeQuest/Main.java
@@ -1,8 +1,17 @@
 package com.CoffeeQuest;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	Scanner input = new Scanner(System.in);
+	Commands cmds = new Commands(input);
+	System.out.println("Welcome");
+		while(true) {
+			System.out.println("What do you want to do next?");
+			System.out.print("> ");
+			cmds.getCommands(input.nextLine());
+		}
     }
 }
