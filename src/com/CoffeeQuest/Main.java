94e6baa61bd7222bea5f7df59d93e2e0429fabf0
Jared-Wad <89890283+Jared-Wad@users.noreply.github.com>
2021-09-09 17:58:46 -0500

    Creating a player class

--- src/com/CoffeeQuest/Main.java
+++ src/com/CoffeeQuest/Main.java
@@ -3,8 +3,16 @@ package com.CoffeeQuest;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        //james made a change
-        //Jared also made a change
+
+        CoffeeQuest cfq = new CoffeeQuest();
+        Scanner input = new Scanner(System.in);
+        Commands cmds = new Commands(input);
+        System.out.println("Welcome");
+        while(true)
+        {
+            System.out.println("What do you want to do next?");
+            System.out.print("> ");
+            cmds.getCommands(input.nextLine());
+        }
     }
 }
---
+++ src/com/CoffeeQuest/Player.java
@@ -0,0 +1,80 @@
+package com.CoffeeQuest;
+
+import java.util.ArrayList;
+
+public class Player {
+    //Private variables
+    private Rooms room;
+    private ArrayList<Item> inventory;
+
+    //Constructor
+    public Player(Rooms room)
+    {
+        this.room = room;
+        this.inventory = new ArrayLIst<Item>();
+    }
+
+    //Methods
+
+    //Pick-up an item
+    public void pickUp(Item item)
+    {
+        inventory.add(item);
+    }
+
+    //Drop an item
+    public void drop(Item item)
+    {
+        inventory.remove(item);
+    }
+
+    //Get inventory
+    public ArrayList<Item> getInventory()
+    {
+        return inventory;
+    }
+
+    //Set inventory
+    public void setInventory(ArrayList<Item>inventory)
+    {
+        this.inventory = inventory;
+    }
+
+    //Return room player is currently in
+    public Rooms GetRoom()
+    {
+        return room;
+    }
+
+    //Set the current room as the player's location
+    public void setRoom(Rooms room)
+    {
+        this.room = room;
+    }
+
+    // TO DO: class for using items(to be designed based off of items)
+
+    public void showItems()
+    {
+        System.out.println("Your inventory:");
+        System.out.println();
+
+        //Check to see if there are items in inventory
+        if(inventory.size()==0)
+        {
+            System.out.println("There are no items in your inventory");
+        }
+        else
+        {
+            //Loop through entire inventory, listing each item
+            for(int x=0; x<inventory.size(); x++)
+            {
+                System.out.println(inventory.get(x));
+            }
+        }
+        //Print a blank line to create space for user entry
+        System.out.println();
+    }
+
+}
+
