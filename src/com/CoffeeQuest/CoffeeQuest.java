687fb063c6ed3eda4dbe47f07ea74f7506863123
Seamus Johnson <24597971@nwtc.edu>
2021-09-09 14:12:43 -0500

    CoffeeQuest class

---
+++ src/com/CoffeeQuest/CoffeeQuest.java
@@ -0,0 +1,9 @@
+package com.CoffeeQuest;
+
+public class CoffeeQuest {
+    ConcurrencyRoom concurrencyRoom = new ConcurrencyRoom();
+    ExceptionRoom exceptionRoom = new ExceptionRoom();
+    FinalExamRoom finalExamRoom = new FinalExamRoom();
+    InfiniteLoopRoom infiniteLoopRoom = new InfiniteLoopRoom();
+
+}
