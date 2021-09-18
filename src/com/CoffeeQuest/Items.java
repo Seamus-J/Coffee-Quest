2980262fa5cab07fd399209c3a689fe1f123a56d
Mantis2712 <Alex Morgan>
2021-09-18 03:34:10 -0500

    Adding items class with two options, will discuss further with group as to how to implement

---
+++ src/com/CoffeeQuest/Items.java
@@ -0,0 +1,28 @@
+package com.CoffeeQuest;
+
+public abstract class Items {
+
+    //Name of the Item
+    private String name;
+
+    //Decription of the item
+    private String description;
+
+    // Method to pull the name of the item
+    abstract String getName();
+
+    //Method to pull the description of the item
+    abstract String getDescription();
+
+    //TODO Depending on how we set up the rooms we can use a comparison to check item vs. room either here
+    //or as part of the item itself. We'll figure it out as a group.
+
+    //TODO The other option is to change this from an abstract to just a basic item class with variables to be called into the rooms. I'll have that commented out underneath this.
+}
+
+/*public class items {
+
+    public String name;
+
+    public String description;
+}*/
