b0cab21211230fbf173432af70f07ff63f92ec8f
Billy <47019848+VexA3@users.noreply.github.com>
2021-09-26 18:14:26 -0500

    Moved command filter to Player class, Added getItem function

--- src/com/CoffeeQuest/Commands.java
+++ src/com/CoffeeQuest/Commands.java
@@ -1,64 +1,26 @@
 package com.CoffeeQuest;
 
-import java.lang.reflect.Array;
-import java.util.Locale;
-import java.util.Scanner;
+
+//TODO: When the items are created, create an enum for the switch-case statement to utilize.
+//public enum ItemTypes {
+//
+//}
 
 public class Commands {
 //    Create item array
 //    Items itemsArray = {};
     String[] commands = {"help", "move", "talk", "pickup", "use", "drop", "quit", "where am i"};
     String[] commandDescription = {"Get this list", "Move to the selected room.", "Talk to an NPC", "Pickup an item", "Use a selected Item", "Drop an item", "Quit the game", "Gives brief description of surroundings"};
-    String command = "";
-    String args = "";
-    Scanner input;
     Rooms currentRoom;
 
-    /**
-     * @param userInput
-     */
-    public Commands(Scanner userInput) {
+    public Commands() {
         // do nothing.
-        // Capture user input incase we need it.
-        input = userInput;
-    }
-
-    /**
-     * @param inputCommand
-     */
-    public void getCommands(String inputCommand) {
-        String[] cmdString = inputCommand.split("\\s");
-        command = cmdString[0];
-        switch(command.toLowerCase(Locale.ROOT)) {
-            case "help":
-                if(args != "") {
-                    System.out.println(command + args);
-                } else {
-                    listCommands();
-                }
-                break;
-            case "quit": quit();
-                break;
-            case "where am i": // Setup where am I command
-                break;
-            case "use":
-                break;
-            case "drop":
-                break;
-            case "move":
-                break;
-            case "talk":
-                break;
-            case "pickup":
-                break;
-            default: System.out.println("Didn't understand that command");
-        }
     }
 
     /**
      * @return void
      */
-    private void listCommands() {
+    public void listCommands() {
         System.out.println("List of commands:");
         int i = 1;
         while(i < commands.length) {
@@ -73,8 +35,21 @@ public class Commands {
     /**
      * @return void
      */
-    private void quit() {
+    public void quit() {
         System.out.println("Quitting game....");
         System.exit(0);
+    }
+
+    public Items getItem(String arg) {
+        Item item = new Item();
+        // Return the class of Item used to pickup, drop, or use.
+        switch(arg) {
+            case "Item":
+                item = new Item();
+                break;
+            default: break;
+        }
+
+        return item;
     }
 }
