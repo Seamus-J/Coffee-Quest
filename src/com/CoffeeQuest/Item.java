a909ac7cf6ebbb8bb86e6b66ddb47120a86ddfcd
Billy <47019848+VexA3@users.noreply.github.com>
2021-09-26 18:15:17 -0500

    Finished Item class, this will be refactored to an item type such as shovel.java

--- src/com/CoffeeQuest/Item.java
+++ src/com/CoffeeQuest/Item.java
@@ -1,4 +1,15 @@
 package com.CoffeeQuest;
 
-public class Item {
+public class Item extends Items {
+    private String itemName;
+    private String desc;
+    @Override
+    String getName() {
+        return itemName;
+    }
+
+    @Override
+    String getDescription() {
+        return desc;
+    }
 }
